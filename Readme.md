0b5d657b425576f1d5d3f81873bd0c66dbaea61d
Abhishek Sharma <sharma312006@gmail.com>
2024-03-08 15:39:13 +0530

    Post model Build

--- Readme.md
+++ Readme.md
@@ -7,3 +7,9 @@
 # signin and generating access and refresh Token
 
 # You can refresh your accessToken using refreshToken
+
+# User can update avatar file
+
+# User can Update their account Details
+
+# User can change their password using oldPassword
---
+++ src/models/post.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const postSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Post = mongoose.model("Post", postSchema);
